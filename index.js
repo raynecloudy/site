c630491d7e3ed1e1f3934d3c2484b0556f76e4ea
breakfast <qwertyytheartist@gmail.com>
2025-03-18 18:37:33 -0700

    yeha

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (!localStorage.getItem("dark")) {
+	document.querySelector(".panel").innerHTML += "<p>press 'l' to toggle dark mode!!</p>";
+}
+
 fetch("https://lastfm-last-played.biancarosa.com.br/raynecloudy/latest-song").then(res => res.json().then(json => {
 	document.getElementById("lastfm_title").innerText = json["track"]["name"];
 	document.getElementById("lastfm_title").href = json["track"]["url"];
