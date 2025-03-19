21756d8a8d928ddd427026ead1c160e5f412c92d
rayne <qwertyytheartist@gmail.com>
2025-03-19 20:04:38 +0000

    new search functionality

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ fetch("https://lastfm-last-played.biancarosa.com.br/raynecloudy/latest-song").th
 	document.getElementById("lastfm_artist").innerText = json["track"]["artist"]["#text"];
 	document.getElementById("lastfm_artist").href = `https://www.last.fm/music/${json["track"]["artist"]["#text"]}/`;
 }));
+
+document.querySelector("#search").addEventListener("keyup", (e) => {
+	if (e.key.length === 1 || e.key === "Backspace") {
+		Array.from(document.querySelectorAll("#links > li")).forEach(element => {
+			if (element.querySelector("a").innerText.includes(document.querySelector("#search").value)) {
+				element.style.pointerEvents = "initial";
+				element.style.opacity = "1";
+				element.querySelector("a").tabIndex = "0";
+			} else {
+				element.style.pointerEvents = "none";
+				element.style.opacity = "0.4";
+				element.querySelector("a").tabIndex = "-1";
+			}
+		});
+	}
+});
