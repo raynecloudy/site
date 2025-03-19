b21478793104abfdc8e2fe7e0362e73557516ed8
breakfast <qwertyytheartist@gmail.com>
2025-03-18 17:11:05 -0700

    dark mode

---
+++ lights.js
@@ -0,0 +1,19 @@
+if (localStorage.getItem("dark") === "true") {
+	document.body.classList.add("dark");
+}
+
+let holding = false;
+
+document.addEventListener("keydown", (e) => {
+	if (e.key = "l" && !e.altKey && !e.shiftKey && !e.ctrlKey && holding === false) {
+		holding = true;
+		document.body.classList.toggle("dark");
+		localStorage.setItem("dark", document.body.classList.contains("dark"));
+	}
+});
+
+document.addEventListener("keyup", (e) => {
+	if (e.key = "l" && !e.altKey && !e.shiftKey && !e.ctrlKey) {
+		holding = false;
+	}
+});
