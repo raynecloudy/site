739faeec79db724a4ba7fb2eeaa981c4f0227e6f
breakfast <qwertyytheartist@gmail.com>
2025-03-21 21:58:33 -0700

    fix capitalization issue

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 let q = new URLSearchParams(window.location.search).get("q");
 
 if (q) {
-	location.href = q.includes(".") ? q : `${q.replace(/ /g, "_")}.html`;
+	location.href = q.includes(".") ? q : `${q.replace(/ /g, "_").toLowerCase()}.html`;
 } else {
 	location.href = "/";
 }
