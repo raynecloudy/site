06831d1fc2e9ba6e8fc0596f9e0ce3771da5bd4e
raynecloudy <rayne@raynec.dev>
2025-04-12 12:08:35 -0700

    censoring

--- controls.js
+++ controls.js
@@ -2,13 +2,17 @@ if (localStorage.getItem("dark") === "true") {
 	document.body.classList.add("dark");
 }
 
-let holding = false;
+let holding_l = false;
 
 document.addEventListener("keydown", (e) => {
-	if (e.key === "l" && !e.altKey && !e.shiftKey && !e.ctrlKey && holding === false && document.activeElement.tagName !== "INPUT") {
-		holding = true;
+	if (e.key === "l" && !e.altKey && !e.shiftKey && !e.ctrlKey && holding_l === false && document.activeElement.tagName !== "INPUT") {
+		holding_l = true;
 		document.body.classList.toggle("dark");
 		localStorage.setItem("dark", document.body.classList.contains("dark"));
+	} else if (e.key === "s" && !e.altKey && !e.shiftKey && !e.ctrlKey) {
+		Array.from(document.getElementsByClassName("restricted")).forEach(element => {
+			element.classList.toggle("show");
+		});
 	}
 });
 
@@ -21,6 +25,6 @@ if (document.getElementById("mobile-dark-mode-toggle")) {
 
 document.addEventListener("keyup", (e) => {
 	if (e.key === "l" && !e.altKey && !e.shiftKey && !e.ctrlKey && document.activeElement.tagName !== "INPUT") {
-		holding = false;
+		holding_l = false;
 	}
 });
